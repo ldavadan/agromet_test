5cecbce77441dc09de52ce9c8d905288bf97cc12
ldavadan <loic.davadan@agro-bordeaux.fr>
2018-06-19 07:29:56 +0000

    updating test grid clc and function clc

--- tests/test_grid_clc.R
+++ tests/test_grid_clc.R
@@ -1,11 +1,38 @@
+#+ preparation, echo=FALSE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
+
+# Avoid interference with old variables by cleaning the Global Environment
+rm(list=ls(all=TRUE))
+
+# Automagically set the wd and the root of all projects 
+if (!require("here")) install.packages("here")
+library(here)
+wd.chr <- here::here()
+
+# loading the library to manage all the other libraries
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+requiredPackages <- read.csv("./settings/requiredPackages.csv", quote = "", sep = ",", header=TRUE, stringsAsFactors=FALSE)
+p_load(char=requiredPackages$packageName, character.only=TRUE )
+p_loaded()
+
+# Dynamic Sourcing of all the required functions
+source(paste0("../R-utilities/R-utilities.R"))
+source_files_recursively.fun("./R")
+source_files_recursively.fun("../agrometeor-public/R/")
+
+
 # Get CLC from Wallonia
 corine.wal.simple.sf <- get_clc_wal()
+
+# load("~/Documents/code/agromet-tests/data-raw/wal_grid_1km.sf.rda")
+# Create column area of each cell
 wal_grid_1km.sf <- wal_grid_1km.sf %>%
   dplyr::mutate(AREA = sf::st_area(.))
 
 # st_crs(corine.wal.simple.sf) <- lambert2008.crs
 # Extract CLC for each cell from the grid
 # https://github.com/r-spatial/sf/issues/347
+# install.packages("lwgeom")
 library(lwgeom)
 class.clip.sf <- sf::st_intersection(lwgeom::st_make_valid(corine.wal.simple.sf), wal_grid_1km.sf)
 
@@ -36,10 +63,10 @@ rgdal::writeOGR(class.clip.final.sp, dsn = './data-output/test', layer = 'class_
 
 
 # create table more lisible
-class.clip.final.df <- base::data.frame(class.clip.final.sf)
-class.clip.clean.df <- class.clip.final.df %>%
+class.clip.clean.df <- base::data.frame(class.clip.final.sf) %>%
   dplyr::select(sid, CLASS, rate_cover) %>%
   reshape2::dcast(sid ~ CLASS, fun = base::sum)
+
 
 
 
