513f8198db3b65432e34f35c5ed9df154d44b218
ldavadan <loic.davadan@agro-bordeaux.fr>
2018-06-20 06:41:07 +0000

    updating names in test grid clc

--- tests/test_grid_clc.R
+++ tests/test_grid_clc.R
@@ -27,43 +27,44 @@ corine.wal.simple.sf <- get_clc_wal()
 # load("~/Documents/code/agromet-tests/data-raw/wal_grid_1km.sf.rda")
 # Create column area of each cell
 wal_grid_1km.sf <- wal_grid_1km.sf %>%
-  dplyr::mutate(AREA = sf::st_area(.))
+  dplyr::mutate(area_cell = sf::st_area(.))
 
 # st_crs(corine.wal.simple.sf) <- lambert2008.crs
 # Extract CLC for each cell from the grid
 # https://github.com/r-spatial/sf/issues/347
 # install.packages("lwgeom")
 library(lwgeom)
-class.clip.sf <- sf::st_intersection(lwgeom::st_make_valid(corine.wal.simple.sf), wal_grid_1km.sf)
+vs.grid.1km.clc.wallonia.polygons_sf <- sf::st_intersection(lwgeom::st_make_valid(corine.wal.simple.sf), wal_grid_1km.sf)
 
 # create a new column with custom id and select columns
-class.clip.sf <- dplyr::mutate(class.clip.sf,
+vs.grid.1km.clc.wallonia.polygons_sf <- dplyr::mutate(vs.grid.1km.clc.wallonia.polygons_sf,
                                         customID = base::paste0("poly_",
-                                                                base::seq_along(1:base::nrow(class.clip.sf)))) %>%
-  dplyr::select(ID, Area_ha, Shape_Leng, Shape_Area, CLASS, sid, customID, AREA, geometry)
+                                                                base::seq_along(1:base::nrow(vs.grid.1km.clc.wallonia.polygons_sf)))) %>%
+  dplyr::select(ID, Area_ha, Shape_Leng, Shape_Area, CLASS, sid, customID, area_cell, geometry)
 
 # create new column with area of each polygon for every cell
-class.clip.sf.summary <- dplyr::group_by(class.clip.sf, customID) %>% 
+vs.grid.1km.poly_area_per_cell.clc.wallonia.polygons_sf <- dplyr::group_by(vs.grid.1km.clc.wallonia.polygons_sf, customID) %>% 
   dplyr::summarise() %>%
   dplyr::mutate(common_area = sf::st_area(.))
 
 # create a column with percentage of cover of every CLC in each cell
-class.clip.final.sf <- sf::st_join(x = class.clip.sf, y = class.clip.sf.summary, join = sf::st_covered_by) %>%
+vs.grid.1km.intersect.clc.wallonia.polygons_sf <- sf::st_join(x = vs.grid.1km.clc.wallonia.polygons_sf, y = vs.grid.1km.poly_area_per_cell.clc.wallonia.polygons_sf, join = sf::st_covered_by) %>%
   dplyr::mutate(rate_cover = base::as.numeric(common_area/AREA * 100)) %>%
   dplyr::select(sid, CLASS, common_area, rate_cover)
 
 
+
 ##### extract
-class.clip.final.sp <- as(class.clip.final.sf, "Spatial")
-class.clip.final.sp$common_area <- base::as.numeric(class.clip.final.sp$common_area)
+vs.grid.1km.intersect.clc_wallonia.polygons_sp <- as(vs.grid.1km.intersect.clc.wallonia.polygons_sf, "Spatial")
+vs.grid.1km.intersect.clc_wallonia.polygons_sp$common_area <- base::as.numeric(vs.grid.1km.intersect.clc_wallonia.polygons_sp$common_area)
 library(rgdal)
 # write shapefile
 # https://stackoverflow.com/questions/13926811/export-a-polygon-from-an-r-plot-as-a-shapefile
-rgdal::writeOGR(class.clip.final.sp, dsn = './data-output/test', layer = 'class_grid_clc', driver = "ESRI Shapefile")
+rgdal::writeOGR(vs.grid.1km.intersect.clc_wallonia.polygons_sp, dsn = './data-output/test', layer = 'vs.grid.1km.intersect.clc_wallonia.polygons', driver = "ESRI Shapefile")
 
 
 # create table more lisible
-class.clip.clean.df <- base::data.frame(class.clip.final.sf) %>%
+vs.grid.1km.intersect.clc_wallonia.df <- base::data.frame(vs.grid.1km.intersect.clc_wallonia.polygons_sf) %>%
   dplyr::select(sid, CLASS, rate_cover) %>%
   reshape2::dcast(sid ~ CLASS, fun = base::sum)
 
